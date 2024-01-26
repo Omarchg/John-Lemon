78c17d2e53ba6890b952c96161f4288fcb53b62e
Omarchg <omarchaglez@gmail.com>
2024-01-26 12:28:33 +0000

    as

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public GameObject player;
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == player)
+        {
+
+        }
+    }
+}
